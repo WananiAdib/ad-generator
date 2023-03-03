5c4b8ddde290a747f5fcc896f36dffa9052878c0
Adib El Ounani <wananiadib@gmail.com>
2023-03-03 15:36:04 +0800

    connected open ai

--- back-end/app.mjs
+++ back-end/app.mjs
@@ -1,6 +1,14 @@
 import express from "express";
+import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from 'dotenv'; 
 
 const app = express();
+
+// Connecting to the OpenAI api
+const configuration = new Configuration({
+	apiKey: process.env.OPEN_AI_KEY,
+})
+const openai = new OpenAIApi(configuration);
 
 app.listen(
 	process.env.PORT || 5000,
