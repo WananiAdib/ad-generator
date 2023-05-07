1e5ad21ea2fb2b592a1bcfee0bcff42e0b012e13
nielnyu <ndc338@nyu.edu>
2023-05-07 19:22:02 +0800

    New Front

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -6,38 +6,20 @@ import Button from './Component/Button'
 
 function App() {
 
-  const [date, setDate] = useState("")
-  const [location, setLocation] = useState("")
-  const [description, setDescription] = useState("")
-  const [qr, setQr] = useState ("Yes")
+  const [prompt, setPrompt] = useState("")
 
-  //handle Date Input
-  const handleDate = (e) =>{
-    setDate(e.target.value)
-  }
-
-  //handle Location Input 
-  const handleLocation = (e) =>{
-    setLocation(e.target.value)
-  }
-
-  //handle Description Input
-  const handleDescription = (e) =>{
-    setDescription(e.target.value)
-  }
-
-  //handle QR code
-  const handleQr = (e) =>{
-    setQr(e.target.value)
+  //handle prompt Input
+  const handlePrompt = (e) =>{
+    setPrompt(e.target.value)
   }
 
   //handle Submission 
   const handleSubmit = async (e) =>{
     e.preventDefault()
-    const posterInfo = {date, location, description, qr}; //delete this when server is running
+    const posterInfo = {prompt}; //delete this when server is running
     //add post request axios in here 
     // try {
-    //   const resp = await axios.post("/", {date:date, location:location, description: description, qr:qr});
+    //   const resp = await axios.post("/", {prompt:prompt});
     //   console.log(resp.data)
     // }
     // catch (error){
@@ -49,21 +31,10 @@ function App() {
   return (
     <div>
       <form onSubmit={handleSubmit}>
-      <label>Date:</label>
-        <TextBox  prompt = "Please Enter Date" onChange = {handleDate}/> 
-        <label>Location:</label>
-        <TextBox prompt = "Please Enter Location of Event" onChange = {handleLocation}/> 
-        <label>Brief Description of Event:</label>
-        <TextBox prompt = "Pleaser Enter Brief Description of Event" onChange ={handleDescription}/> 
-        <label>QR Code</label>
-        <select onChange = {handleQr}>
-          <option value = "Yes">Yes</option>
-          <option value= "No">No</option>
-        </select>
+      <label>Prompt:</label>
+        <TextBox  prompt = "Please Enter A prompt" onChange = {handlePrompt}/> 
         <Button name = "Submit"></Button>
       </form>
-
-
     </div>
   )
 }
